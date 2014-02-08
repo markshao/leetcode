e6156529e837a7c5cd457227957367608e573c79
markshao <37647536@qq.com>
2014-02-09 00:10:01 +0800

    time exceed

---
+++ src/leetcode/oj/wordLadder/Solution.java
@@ -0,0 +1,46 @@
+package leetcode.oj.wordLadder;
+
+import java.util.HashSet;
+
+//http://oj.leetcode.com/problems/word-ladder/
+
+public class Solution {
+
+	static int count = 0;
+
+	public int ladderLength(String start, String end, HashSet<String> dict) {
+		ladder(start, end, dict, new HashSet<String>());
+		return count;
+	}
+
+	public void ladder(String start, String end, HashSet<String> dict,
+			HashSet<String> path) {
+		if (cmp(start, end) == 1) {
+			int c = path.size() + 2;
+			if (count == 0)
+				count = c;
+			else if (c < count)
+				count = c;
+		} else {
+			for (String s : dict) {
+				if (cmp(start, s) == 1) {
+					HashSet<String> newdict = (HashSet<String>) dict.clone();
+					newdict.remove(s);
+					HashSet<String> newpath = (HashSet<String>) path.clone();
+					path.add(s);
+					ladder(s, end, newdict, newpath);
+				}
+			}
+		}
+	}
+
+	public int cmp(String x, String y) {
+		int length = x.length();
+		int result = 0;
+		for (int i = 0; i < length; i++) {
+			if (x.charAt(i) != y.charAt(i))
+				result++;
+		}
+		return result;
+	}
+}
