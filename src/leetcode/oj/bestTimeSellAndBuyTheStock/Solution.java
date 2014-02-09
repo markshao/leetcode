91b466c30c30141a4f5be5c0abd3caa5991cca0b
markshao <37647536@qq.com>
2014-02-09 19:51:15 +0800

    change the solution

--- src/leetcode/oj/bestTimeSellAndBuyTheStock/Solution.java
+++ src/leetcode/oj/bestTimeSellAndBuyTheStock/Solution.java
@@ -5,44 +5,15 @@ package leetcode.oj.bestTimeSellAndBuyTheStock;
 public class Solution {
 
 	public int maxProfit(int[] prices) {
-		if (prices.length < 2) {
-			return 0;
-		}
-		return find(prices, prices.length - 1);
+	    if (prices.length <= 1) return 0;
+
+	    int profit = 0;
+	    int min = prices[0];
+	    for (int i = 0; i < prices.length; i++) {
+	        min = Math.min(min, prices[i]);
+	        profit = Math.max(profit, prices[i] - min);
+	    }
+	    return profit;
 	}
 
-	public int find(int[] arr, int maxIdx) {
-		if (maxIdx < 1) {
-			return 0;
-		}
-		int min = arr[0];
-		int minIdx = 0;
-		for (int i = 1; i <= maxIdx; i++) {
-			if (arr[i] < min) {
-				min = arr[i];
-				minIdx = i;
-			}
-		}
-
-		// Find right max
-		int maxRight = 0;
-		for (int i = minIdx + 1; i <= maxIdx; i++) {
-			int tmp = arr[i] - min;
-			if (tmp > maxRight) {
-				maxRight = tmp;
-			}
-		}
-		int maxLeft = 0;
-		if (minIdx == 2) {
-			maxLeft = arr[1] - arr[0];
-		} else if (minIdx > 2) {
-			maxLeft = find(arr, minIdx - 1);
-		}
-
-		if (maxRight > maxLeft) {
-			return maxRight;
-		} else {
-			return maxLeft;
-		}
-	}
 }
