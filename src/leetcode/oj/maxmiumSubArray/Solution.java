f53da70a6bb02206a450b76981b6112692468201
markshao <37647536@qq.com>
2014-02-17 14:08:04 +0800

    add code

--- src/leetcode/oj/maxmiumSubArray/Solution.java
+++ src/leetcode/oj/maxmiumSubArray/Solution.java
@@ -2,13 +2,15 @@ package leetcode.oj.maxmiumSubArray;
 
 public class Solution {
     public int maxSubArray(int[] A) {
-        int ans = 0, maxn = Integer.MIN_VALUE;
-        for (int i = 0; i < A.length; i++) {
-            if (ans < 0) ans = 0;
-            ans += A[i];
-            maxn = Math.max(maxn, ans);
+        int max = A[0];
+        int[] sum = new int[A.length];
+        sum[0] = A[0];
+
+        for (int i = 1; i < A.length; i++) {
+            sum[i] = Math.max(A[i], sum[i - 1] + A[i]);
+            max = Math.max(max, sum[i]);
         }
-        return maxn;
+        return max;
     }
 
     public static void main(String[] args) {
