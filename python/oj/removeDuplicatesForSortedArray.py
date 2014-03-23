641137504bad41e775d62dbe292eb38eaf112225
markshao <37647536@qq.com>
2014-03-23 23:41:21 +0800

    http://oj.leetcode.com/problems/pascals-triangle/

---
+++ python/oj/removeDuplicatesForSortedArray.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+#coding:utf8
+
+__author__ = 'markshao'
+
+
+class Solution:
+    # @param a list of integers
+    # @return an integer
+    def removeDuplicates(self, A):
+        pass
